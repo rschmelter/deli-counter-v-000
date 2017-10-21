386dc940cecc43976b124dc7788a0589cdecc255
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-21 19:31:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,9 @@ def line(katz_deli)
 if katz_deli.empty?
   puts "The line is currently empty."
 else
-  puts "The line is currently:"
+  message = "The line is currently:"
    katz_deli.each do |name|
-     puts "#{name.index + 1} #{name}"
+    message += "#{name.index + 1} #{name}"
     end
   end
 end
